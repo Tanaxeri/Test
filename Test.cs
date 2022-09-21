bdf43a2bdda15d30fa848adf3fc73396d1757f02
Tanaxeri <tamasgamer@gmail.hu>
2022-09-21 12:52:13 +0200

    2.0

--- Test.cs
+++ Test.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace Test
 {
     class Test
@@ -11,8 +11,11 @@ namespace Test
         static void Main(string[] args)
         {
 
+            int EgyesEgeszSzam = Convert.ToInt32(Console.ReadLine());
+            int KettesEgeszSzam = Convert.ToInt32(Console.ReadLine());
 
-            
+            Console.WriteLine("\nProgram vége");
+            Console.ReadKey();
         }
     }
 }
